4fd3b84e285066477adb16c075c508e3839abceb
epashiq <ashiqashi4715@gmail.com>
2025-01-13 16:28:48 +0530

    .

--- lib/features/order/presentation/view/widgets/order_card.dart
+++ lib/features/order/presentation/view/widgets/order_card.dart
@@ -1,36 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:food_crm/general/utils/color_const.dart';
-import 'package:food_crm/main.dart';
 
 class OrderCard extends StatelessWidget {
   final String itemName;
   final String quantity;
   final String rate;
   final int listCount;
-  final String? total;
 
   const OrderCard({
     super.key,
-    required this.itemName,
+    required this.itemName, // Customizable row text
     required this.quantity,
-    required this.rate,
-    required this.listCount,
-    this.total,
+    required this.rate, required this.listCount,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width * 1,
+      width: 340,
       decoration: BoxDecoration(
-        color: ClrConstant.greyColor,
+        color: ClrConstant.greyColor, 
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
         children: [
           Container(
             height: 50,
-            width: width * 1,
+            width: 340,
             decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
@@ -43,70 +39,51 @@ class OrderCard extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Item"),
+                  Text("Item"), // Static text
                   Row(
                     children: [
-                      Text("Qty"),
+                      Text("Qty"), // Static text
                       SizedBox(width: 20),
-                      Text("Rate"),
+                      Text("Rate"), // Static text
                     ],
                   ),
                 ],
               ),
             ),
           ),
+          // Customizable Row Content
           ListView.builder(
-            itemCount: listCount,
+            itemCount:listCount ,
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (BuildContext context, int index) {
+            itemBuilder: (BuildContext context, int index) { 
               return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(itemName),
-                        SizedBox(
-                          width: 70,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(quantity),
-                              Text("₹$rate"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-          if (total != null) ...[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const SizedBox(),
-                  Text(
-                    "₹$total",
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
+                  Text(itemName), // Customizable
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: SizedBox(
+                      width: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(quantity), // Customizable
+                         
+                          Text(rate), 
+                          
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-          const SizedBox(
-            height: 12,
-          )
+            );
+            },
+           
+          ),
         ],
       ),
     );
